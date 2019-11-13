248e9dfcb9a0017c71355fa5c2c2d24abc585d07
Felipe G. Almeida <felipegalmeida@outlook.com>
2019-11-13 11:59:05 -0300

    First commit - the beginning

---
+++ app/src/main/java/fgapps/com/br/iassistant2/MainActivity.kt
@@ -0,0 +1,12 @@
+package fgapps.com.br.iassistant2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
